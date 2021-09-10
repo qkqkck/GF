1ec2fee584575cd65f535c78a0f6ceff4f358f2b
20171221 민석 <90017906+qkqkck@users.noreply.github.com>
2021-09-10 04:12:26 +0000

    SDL separation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 
 SDL_Window* gpwindow = 0;
 SDL_Renderer* gpRenderer = 0;
+bool gbRunning = false;
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
+
 {
   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
   {
-    gpwindow = SDL_CreateWindow("Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    gpwindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
 
     if ('gpwindow ! = 0')
     {
@@ -16,16 +18,42 @@ int main(int argc, char* args[])
   }
   else
   {
-    return 1;
+    return false;
   }
 
-  SDL_SetRenderDrawColor(gpRenderer, 0, 0, 255, 255);
+  SDL_SetRenderDrawColor(gpRenderer, 255, 255, 0, 255);
+
+  return true;
+}
+
+void render()
+{
   SDL_RenderClear(gpRenderer);
   SDL_RenderPresent(gpRenderer);
-
   SDL_Delay(5000);
   SDL_Quit();
+}
 
+int main(int argc, char* argb[])
+{
+  if(init("Breaking UP HelloSDL",
+  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,480,
+  SDL_WINDOW_SHOWN))
+  {
+    gbRunning = true;
+  }
+  else
+  {
+    return 1;
+  }
+  {
+    while(gbRunning)
+
+    render();
+
+  }
+
+  SDL_Quit();
 
   return 0;
 }
