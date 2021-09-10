3da9e48bf5328a6606781dd947b48a029c49187c
20171221 민석 <90017906+qkqkck@users.noreply.github.com>
2021-09-10 02:24:58 +0000

    SDL

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
-#include <stdio.h>
+#include <SDL.h>
+#include<iostream>
+
+SDL_Window* gpwindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
-  int a = 0;
-  int b = 0;
+  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow("Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    if (g_pWindow ! = 0)
+    {
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+    }
+  }
+  else
+  {
+    return 1;
+  }
 
-  scanf("%d + %d", &a, &b);
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
 
-  printf( "%d\n", a + b);
-  
+  SDL_Delay(5000);
+  SDL_Quit();
 
 
   return 0;
