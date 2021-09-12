7390efccb644a3c667538b1d1a7944a5461d9c56
20171221 민석 <90017906+qkqkck@users.noreply.github.com>
2021-09-12 18:03:49 +0000

    SDL changeColor

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <SDL.h>
+#include <iostream>
 
-SDL_Window* gpwindow = 0;
-SDL_Renderer* gpRenderer = 0;
-bool gbRunning = false;
+
+
+SDL_Window* g_pwindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+bool g_pRunning = false;
+int lastTime = 0, currentTime; // 색변환시간
 
 bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 
 {
   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
   {
-    gpwindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
+    g_pwindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
 
-    if ('gpwindow ! = 0')
+    if (g_pwindow != 0)
     {
-      gpRenderer = SDL_CreateRenderer (gpwindow, -1, 0);
+      g_pRenderer = SDL_CreateRenderer (g_pwindow, -1, 0);
     }
   }
   else
@@ -21,17 +25,41 @@ bool init(const char* title, int xpos, int ypos, int height, int width, int flag
     return false;
   }
 
-  SDL_SetRenderDrawColor(gpRenderer, 255, 255, 0, 255);
+  SDL_SetRenderDrawColor(g_pRenderer, 255, 255, 0, 255);
 
   return true;
 }
 
+void changeColor()
+{
+  
+    currentTime = SDL_GetTicks();
+
+    if (currentTime > lastTime + 1000)
+
+    {
+       SDL_SetRenderDrawColor(g_pRenderer, rand() % 255, rand() % 255, rand() % 255, 255);
+
+      lastTime = currentTime;
+
+    }
+
+}
+
+void update()
+{
+  
+  changeColor();
+
+}
+
+
 void render()
 {
-  SDL_RenderClear(gpRenderer);
-  SDL_RenderPresent(gpRenderer);
-  SDL_Delay(5000);
-  SDL_Quit();
+  
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+  
 }
 
 int main(int argc, char* argb[])
@@ -40,16 +68,22 @@ int main(int argc, char* argb[])
   SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,480,
   SDL_WINDOW_SHOWN))
   {
-    gbRunning = true;
+    g_pRunning = true;
   }
   else
   {
     return 1;
   }
   {
-    while(gbRunning)
 
-    render();
+    while(g_pRunning)
+
+    {
+
+    update(); //업데이트
+    render(); //출력
+
+    }
 
   }
 
