017629ce9174c344bb2810d442278fcff4a3e256
20171221 민석 <90017906+qkqkck@users.noreply.github.com>
2021-09-17 05:08:22 +0000

    BMP

---
+++ Game.cpp
@@ -0,0 +1,84 @@
+#include"Game.h"
+
+SDL_Window* m_pWindow = 0;
+SDL_Renderer* m_pRenderer = 0;
+bool m_bRunning = false;
+
+bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+  {
+    m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+    if (m_pWindow !=0){
+      m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+      if (m_pRenderer !=0){
+        SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+
+  m_bRunning = true;
+  return true;
+
+}
+
+void Game::update()
+{
+    
+ SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+
+ m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+ SDL_FreeSurface(pTempSurface);
+
+ SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+ m_destinationRectangle.w = m_sourceRectangle.w;
+ m_destinationRectangle.h = m_sourceRectangle.h;
+
+ m_destinationRectangle.x = m_sourceRectangle.x = 0;
+ m_destinationRectangle.y = m_sourceRectangle.y = 0;
+}
+
+void Game::render()
+{
+  
+  SDL_RenderClear(m_pRenderer);
+  SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+  SDL_RenderPresent(m_pRenderer);
+  
+}
+
+bool Game::running()
+{
+ return m_bRunning;
+}
+
+void Game::handleEvents()
+{
+  SDL_Event event;
+  if(SDL_PollEvent(&event))
+  {
+    switch (event.type)
+    {
+      case SDL_QUIT:
+      m_bRunning = false;
+      break;
+      default:
+      break;
+    }
+  }
+}
+
+void Game::clean()
+{
+  SDL_DestroyWindow(m_pWindow);
+  SDL_DestroyRenderer(m_pRenderer);
+  SDL_Quit();
+}
--- main.cpp
+++ main.cpp
@@ -1,93 +1,18 @@
-#include <SDL.h>
-#include <iostream>
+#include"Game.h"
 
+Game* g_game = 0;
 
-
-SDL_Window* g_pwindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_pRunning = false;
-int lastTime = 0, currentTime; // 색변환시간
-
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
-
+int main(int argc, char* argv[])
 {
-  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  g_game = new Game();
+  g_game->init("Game class", 100, 100, 640, 480, 0);
+  while (g_game->running())
   {
-    g_pwindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
-
-    if (g_pwindow != 0)
-    {
-      g_pRenderer = SDL_CreateRenderer (g_pwindow, -1, 0);
-    }
+    g_game->handleEvents();
+    g_game->update();
+    g_game->render();
   }
-  else
-  {
-    return false;
-  }
-
-  SDL_SetRenderDrawColor(g_pRenderer, 255, 255, 0, 255);
-
-  return true;
-}
-
-void changeColor()
-{
-  
-    currentTime = SDL_GetTicks();
-
-    if (currentTime > lastTime + 1000)
-
-    {
-       SDL_SetRenderDrawColor(g_pRenderer, rand() % 255, rand() % 255, rand() % 255, 255);
-
-      lastTime = currentTime;
-
-    }
-
-}
-
-void update()
-{
-  
-  changeColor();
-
-}
-
-
-void render()
-{
-  
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
-  
-}
-
-int main(int argc, char* argb[])
-{
-  if(init("Breaking UP HelloSDL",
-  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,480,
-  SDL_WINDOW_SHOWN))
-  {
-    g_pRunning = true;
-  }
-  else
-  {
-    return 1;
-  }
-  {
-
-    while(g_pRunning)
-
-    {
-
-    update(); //업데이트
-    render(); //출력
-
-    }
-
-  }
-
-  SDL_Quit();
-
+  g_game->clean();
   return 0;
+
 }
